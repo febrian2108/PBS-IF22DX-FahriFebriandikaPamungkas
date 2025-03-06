8d2191885e0fba697d8a4721652c79c229d9dc2c
Fahri <febrian2104@gmail.com>
2025-03-06 20:02:29 +0700

    master

---
+++ backend/src/app/api/user/route.ts
@@ -0,0 +1,25 @@
+import { metadata } from "@/app/layout";
+import { PrismaClient } from "@prisma/client";
+import { error } from "console";
+import { NextRequest, NextResponse } from "next/server";
+
+// buat variabel prisma
+const prisma = new PrismaClient();
+
+// buat fungsi service "Get" (tb_user)
+
+export const GET = async () => {
+    // menampilkan data dari user
+    const view = await prisma.user.findMany({});
+    // proses atau respon api
+    return NextResponse.json({
+        metaData: {
+            error: false,
+            message: "Data user berhasil ditampilkan",
+            list: "Data User",
+        },
+        data_user: view,
+    },{
+        status: 200
+    });
+};

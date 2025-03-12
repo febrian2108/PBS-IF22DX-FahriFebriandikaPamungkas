77fb3636153da67ef4809c2a5af1c695c3f10796
Fahri <febrian2104@gmail.com>
2025-03-12 20:42:38 +0700

    tambah data, enkripsi, dekripsi, sorting username agar tidak ada username yang sama

--- backend/src/app/api/user/route.ts
+++ backend/src/app/api/user/route.ts
@@ -2,6 +2,7 @@ import { metadata } from "@/app/layout";
 import { PrismaClient } from "@prisma/client";
 import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
+import { genSaltSync, hashSync } from "bcrypt-ts";
 
 // buat variabel prisma
 const prisma = new PrismaClient();
@@ -11,28 +12,89 @@ export const GET = async () => {
     // menampilkan data dari user
     const view = await prisma.user.findMany({});
     // jika data user tidak ditemukan
-    if (view.length === 0) {
-        return NextResponse.json({
-            metaData: {
-                error: true,
-                message: "Data user tidak ditemukan",
-                list: "Data User",
-                status: 404,
+    if (view.length == 0) {
+        return NextResponse.json(
+            {
+                metaData: {
+                    error: true,
+                    message: "Data user tidak ditemukan",
+                    list: "Data User",
+                    status: 404,
+                },
+                data_user: view,
             },
-            data_user: view,
-        }, {
-            status: 404
-        });
-        }
+            {
+                status: 404,
+            }
+        );
+    }
     // proses atau respon api
     return NextResponse.json({
-        metaData: {
-            error: false,
-            message: "Data user berhasil ditampilkan",
-            list: "Data User",
+            metaData: {
+                error: false,
+                message: "Data user berhasil ditampilkan",
+                list: "Data User",
+            },
+            data_user: view,
         },
-        data_user: view,
-    },{
-        status: 200
+        {
+            status: 200,
+        }
+    );
+};
+
+//buat fungsi service "Post" (tb_user)
+export const POST = async (request: NextRequest) => {
+    //buat object untuk data isian
+    const { nama_value, username_value, password_value } = await request.json();
+
+    //cek apakah ada userame yang sama di database
+    const cek = await prisma.user.findMany({
+        where: {
+            username: username_value,
+        },
     });
+
+    // jika data username di temukan
+    if (cek.length >= 1) {
+        return NextResponse.json(
+            {
+                metaData: {
+                    error: true,
+                    message: "Data User Gagal disimpan! Username sudah digunakan",
+                    status: 409,
+                },
+                data_user: cek,
+            },
+            {
+                status: 409,
+            }
+        );
+    }
+
+    //enkripsi password
+    const salt = genSaltSync(5);
+    const encrypt = hashSync(password_value, salt);
+
+    //simpan data ke database
+    const save = await prisma.user.create({
+        data: {
+            nama: nama_value,
+            username: username_value,
+            password: encrypt,
+        },
+    });
+
+    //proses atau respon api
+    return NextResponse.json(
+        {
+            metaData: {
+                error: false,
+                message: "Data user berhasil Disimpan",
+                status: 201,
+            },
+        },{
+            status: 201,
+        }
+    );
 };
